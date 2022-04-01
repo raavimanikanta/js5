1b3e7c956fd3271880d8fa64a74a7948a0b22705
raavimanikanta <raavi,ani123@gmail.com>
2022-04-01 15:12:16 +0530

    first changes

---
+++ script.js
@@ -0,0 +1,28 @@
+var Day="monday";
+switch (Day) {
+    case "monday":
+    console.log("The day is monday");
+    break;
+    case "Tuesday":
+    console.log("The day is Tuesday");
+    break;
+    case "wedday":
+    console.log("The day is Wednesday");
+    break;
+    case "thursday":
+    console.log("The day is Thursday");
+    break;
+    case "friday":
+    console.log("The day is Friday");
+    break;
+    case "saturday":
+    console.log("The day is saturday");
+    break;
+
+    default:
+    break;
+}
+
+for(i=1;i<=10;i++) {
+    console.log(`13*${i}=${13*i}`)
+}
